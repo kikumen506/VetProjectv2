9d9e02de13c3ed2420fca0d07b1c14b09d58c0da
Kikumen <kikumen91@gmail.com>
2019-03-09 19:55:40 +0100

    partedel menu de navegacion

--- front/vetProjectV2/src/app/app.module.ts
+++ front/vetProjectV2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
